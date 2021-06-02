07ba906239d835bf0b6d7a715fda6b06ad713638
Shivam2799 <shivam.sharma@appinventiv.com>
2021-06-02 10:51:53 +0530

    added css file to home module

--- training/src/module/home/indec.js
+++ training/src/module/home/indec.js
@@ -4,7 +4,7 @@ class Home extends Component{
 render() {
     return (
         <div className={"homeContainer"}>
-            <h1>className={"h1tag"}> {"hello"}</h1>
+            <h1 className={"h1tag"}> {"hello"}</h1>
         </div>
     );
 }
